94af40861e2de61f70c724818bc753578f0108f2
Shustrova-MP <shustrovamp@untech.rshb.ru>
2024-10-15 10:26:04 +0300

    add basic project structure

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import svgrPlugin from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    plugins: [
+        react({
+            jsxRuntime: 'automatic',
+        }),
+        dts(),
+        tsconfigPaths(),
+        svgrPlugin({
+            svgrOptions: {
+                icon: true,
+            },
+        }),
+    ],
+    resolve: {
+        alias: [{ find: /^~/, replacement: '' }],
+    },
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true,
+            },
+            // scss: {
+            //     additionalData: `@import "src/styles/variables.scss";`, // если нужно автоматически импортировать переменные
+            // },
+        },
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.tsx'),
+            name: 'corporate-ui-kit',
+            formats: ['es'],
+            fileName: 'index',
+        },
+        // sourcemap: true, // ✅
+        minify: true,
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+        },
+    },
+})
